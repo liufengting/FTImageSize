7727bf2462a8b4df4b87cf004876738f198fe98b
liufengting <wo157121900@me.com>
2016-12-02 21:51:05 +0800

    ,,

---
+++ FTImageSize/FTImageSize.h
@@ -0,0 +1,19 @@
+//
+//  FTImageSize.h
+//  FTImageSize
+//
+//  Created by liufengting on 02/12/2016.
+//  Copyright © 2016 LiuFengting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FTImageSize.
+FOUNDATION_EXPORT double FTImageSizeVersionNumber;
+
+//! Project version string for FTImageSize.
+FOUNDATION_EXPORT const unsigned char FTImageSizeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FTImageSize/PublicHeader.h>
+
+
